2bbce8032ba33ad6361a7de71d97d31aa7de51f2
John Barton (joho) <jrbarton@gmail.com>
2012-12-17 12:45:30 +1100

    COMPUTER BLUE.

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source :rubygems
+
+gem 'thin'
+gem 'compass'
+gem 'sinatra'
+gem 'haml'
+gem 'sass'
+gem 'maruku'
+gem 'coffee-script'
+
+group :development do
+  gem 'heroku'
+  gem 'foreman'
+end
---
+++ app.rb
@@ -0,0 +1,27 @@
+# app.rb
+require 'compass'
+require 'sinatra'
+require 'haml'
+require 'sass'
+require 'maruku'
+require 'coffee-script'
+
+configure do
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
+end
+
+set :public_folder, 'public'
+
+get '/' do
+  haml :index
+end
+
+get '/stylesheets/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options)
+end
+
+get '/javascripts/:name.js' do
+  content_type 'text/javascript'
+  coffee :"/javascripts/#{params[:name]}"
+end
